2d75a08911ff37460049c63efc94ab310afb3533
Vahan92 <vahan.kalenteryan@gmail.com>
2018-11-21 12:24:40 +0400

    Code cleanup.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const authRoutes = require('./routes/login');
 const validator = require('express-validator');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const signup = require('./routes/signup');
-const login = require('./routes/login');
+const routes = require('./routes/auth-routes');
 const keys = require('./config/keys');
 
 const app = express();
@@ -13,18 +11,17 @@ app.use(validator());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-mongoose.connect(keys.mongodb.URI, ()=>{
-})
+mongoose.connect(keys.mongodb.URI, () => {});
 
-app.use('/confirmation', signup.confirmationPost);
-app.use('/sign-up', signup.signupPost);
-app.use('/login',signup.loginPost);
-app.post('/resend', signup.resendTokenPost);
+app.use('/confirmation', routes.confirmationPost);
+app.use('/sign-up', routes.signupPost);
+app.use('/login',routes.loginPost);
+app.post('/resend', routes.resendTokenPost);
 
 app.set('port', (process.env.PORT || 3001));
 
 app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+	console.log('Server started on port '+ app.get('port'));
 });
 
 module.exports = app;
